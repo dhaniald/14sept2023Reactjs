bd0cf5cefa61086a0992497f97280d0d60063294
Gaurav <gtiwari95@gmail.com>
2023-09-14 22:31:52 +0530

    v1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+//1- import area
+
+ 
+import './App.css';
+import header from './components/header';
+import footer from './components/footer';
+import aside from './components/aside';
+import section from './components/section';
+//2 - function defination area
+function App() {
+  return (
+   <> 
+   <header/>
+     <main>
+     <aside/>
+      <section/>
+     </main>
+     <footer />
+   </>
+  );
+}
+//3- export area
+export default App;
